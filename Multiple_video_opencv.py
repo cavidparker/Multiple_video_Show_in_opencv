b753d7e443d98c2151dcb329502c66a36b301be3
cavidparker <cavidpark@gmail.com>
2020-08-25 22:58:27 +0600

    first commit

---
+++ Multiple_video_opencv.py
@@ -0,0 +1,26 @@
+import cv2
+from time import sleep
+
+frame0 = cv2.VideoCapture('test.mp4')
+frame1 = cv2.VideoCapture('Vaseline_Healing.mp4')
+
+while 1 :
+
+    ret0, img0 = frame0.read()
+    ret1, img00 = frame1.read()
+    img1 = cv2.resize(img0,(360,240))
+    img2 = cv2.resize(img00,(360,240))
+
+    if (frame0):
+        cv2.imshow('img1', img1)   
+    if (frame1):
+        cv2.imshow('img2', img2)
+
+
+    k = cv2.waitKey(30) & 0xff
+    if k ==27:
+        break     
+
+frame0.release()
+frame1.release()
+cv2.destroyAllWindows()       
